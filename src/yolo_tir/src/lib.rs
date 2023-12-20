1983142ef077ed1033752868b6719c8155668143
paolo.dimasi <paodimasi@outlook.com>
2023-12-20 23:37:24 +0100

    initial structure for bounding box lib

---
+++ src/yolo_tir/src/lib.rs
@@ -0,0 +1,93 @@
+use std::cmp::Ordering;
+
+#[derive(Copy, Clone, Debug)]
+pub struct BoxInfo{
+    x1:f32,
+    y1:f32,
+    x2:f32,
+    y2:f32,
+    score:f32,
+    label:usize
+}
+
+impl Eq  for BoxInfo{}
+
+// TODO constants to a launch file
+const MODEL_PATH: &str = "param/best_seq_learn.onnx";
+const IMAGE_PATH: &str = "uav_view.jpg";
+
+const CLASS_LABEL: [&str; 4] =  ["person","bike","car","other vehicle"];
+impl Ord for BoxInfo{
+    fn cmp(&self, other: &Self) -> Ordering {
+
+        if self.score < other.score {
+            Ordering::Less
+        }
+
+        else if self.score > other.score {
+            Ordering::Greater
+        }
+
+        else{
+            Ordering::Equal
+        }
+    }
+}
+
+
+impl PartialOrd<Self> for BoxInfo {
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        Some(self.cmp(&other))
+    }
+}
+
+impl PartialEq<Self> for BoxInfo{
+    fn eq(&self, other: &Self) -> bool {
+        self.score == other.score
+    }
+}
+
+impl BoxInfo{
+    fn new(x1:f32, y1:f32, x2:f32, y2:f32, score:f32, label:usize) -> Self {
+        Self { x1, y1, x2, y2, score, label}
+    }
+
+    fn nms(mut input_boxes:Vec<Self>, nms_threshold:f32) -> Vec<Self>{
+        input_boxes.sort_by(|a, b| b.cmp(a));
+        let v_area: Vec<f32> = input_boxes
+            .iter()
+            .map(|box_info| (box_info.x2 - box_info.x1 ) * (box_info.y2 - box_info.y1 ))
+            .collect();
+
+        let mut is_suppressed = vec![false; input_boxes.len()];
+
+        for i in 0..input_boxes.len() {
+            if is_suppressed[i] == false {
+                for j in (i + 1)..input_boxes.len() {
+                    if is_suppressed[j] == false {
+                        let xx1 = f32::max(input_boxes[i].x1, input_boxes[j].x1);
+                        let yy1 = f32::max(input_boxes[i].y1, input_boxes[j].y1);
+                        let xx2 = f32::min(input_boxes[i].x2, input_boxes[j].x2);
+                        let yy2 = f32::min(input_boxes[i].y2, input_boxes[j].y2);
+
+                        let w = f32::max(0.0, xx2 - xx1 );
+                        let h = f32::max(0.0, yy2 - yy1 );
+                        let inter_area = w * h;
+                        let iou = inter_area / (v_area[i] + v_area[j] - inter_area);
+
+                        // remove all predictions with  the desired IoU
+                        if iou >= nms_threshold {
+                            is_suppressed[j] = true;
+                        }
+                    }
+                }
+            }
+        }
+
+        let out_boxes:Vec<BoxInfo> = input_boxes.iter()
+            .enumerate()
+            .filter(|t| !is_suppressed[t.0])
+            .map( |t| *t.1).collect();
+        out_boxes
+    }
+}
