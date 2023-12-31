0754323127118a8996c7ba29924ed29ad304c80f
Pamasi <pamasideveloper@gmail.com>
2023-12-31 16:25:56 +0000

    update ros entrypoint.sh

--- ros_entrypoint.sh
+++ ros_entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# onnx library to shared library path
+# onnx library to shared library path (for no-docker change the path)
 export LD_LIBRARY_PATH=/home/user/library/onnxruntime-linux-x64-1.13.1/lib:$LD_LIBRARY_PATH
 # setup ros2 environment
 . /opt/ros/humble/setup.sh
